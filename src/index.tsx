5d31faf5a05f932e04dba27e4efbe6b2cedf6acc
Chet Corcos <ccorcos@gmail.com>
2021-10-12 17:23:35 -0700

    Repl tests

--- src/index.tsx
+++ src/index.tsx
@@ -1,31 +1,13 @@
 import React from "react"
 import ReactDOM from "react-dom"
-import { AppState } from "./AppState"
-import { AppStorage } from "./AppStorage"
+import { Triplestore } from "triple-database"
 import { App } from "./components/App"
-import { Environment, EnvironmentProvider } from "./Environment"
 import "./index.css"
 
-// Build the environment.
-const storage = new AppStorage()
-const initialState = storage.get()
-
-const app = new AppState(initialState)
-app.addListener(() => storage.set(app.state))
-
-const environment: Environment = { app }
-
-// For debugging from the Console.
-;(window as any)["environment"] = environment
-Object.assign(window as any, environment)
+const db = new Triplestore()
 
 // Render the app.
 const root = document.createElement("div")
 document.body.appendChild(root)
 
-ReactDOM.render(
-	<EnvironmentProvider value={environment}>
-		<App />
-	</EnvironmentProvider>,
-	root
-)
+ReactDOM.render(<App db={db} />, root)
